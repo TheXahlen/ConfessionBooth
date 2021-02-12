3229300955a19c8cc3cdeab096328b0708a89e71
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2021-02-12 09:30:02 -0600

    Create TextHandler.js

---
+++ Pagefiles/Javascript/TextHandler.js
@@ -0,0 +1,11 @@
+function cleartextbox(){
+var textequal = document.getElementById("textinput").value
+if (textequal.length > 300){
+document.getElementById("textinput").value = "text is more than 300";
+}
+else if(textequal.length = 0){
+document.getElementById("textinput").value = "text is 0";
+} else {
+document.getElementById("textinput").value = "";
+}
+document.getElementById("textinput").value = ""     
