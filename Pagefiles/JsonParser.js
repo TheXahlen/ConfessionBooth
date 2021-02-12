5a1fa5f24ddcb60bfa0834058610c4063ffa8b64
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2021-02-12 09:25:13 -0600

    Create JsonParser.js

---
+++ Pagefiles/JsonParser.js
@@ -0,0 +1,31 @@
+var confessionlist = []
+
+async function apicall(){
+    return new Promise(async(resolve,reject)=>{
+        let url = 'https://spreadsheets.google.com/feeds/cells/1arXjPy5iUERug2miLIPZKs0equJKxcT-QMqika16gm0/1/public/full?alt=json';
+        let obj = await fetch(url)
+        resolve(obj.json())
+    })
+
+}
+
+async function load() {
+	var anything = await apicall()
+    console.log(anything)
+    var textjs = anything.feed.entry
+    var yes = textjs["id"]
+    for (let i = 1; i < textjs.length; i++) {
+    var confessionentry = textjs[i].content.$t
+    var confessioncomp = "[ENTRY NUMBER: " + i + " " + confessionentry + "]" 
+    confessionlist.push(confessioncomp)
+    console.log(confessionlist)    
+    
+
+}
+var lengthoflist = confessionlist.length
+console.log(lengthoflist)
+var listselect = Math.floor(Math.random() * lengthoflist)
+console.log(listselect)
+document.getElementById("json").innerHTML = confessionlist[listselect]
+
+}
