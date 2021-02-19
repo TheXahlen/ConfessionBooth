b9a6d1228586a2c1402fdffee9ee37f595b01dbf
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2021-02-18 19:33:35 -0600

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -1,7 +1,7 @@
 
 async function apicall(){
 	return new Promise(async(resolve,reject)=>{
-	let url = 'https://spreadsheets.google.com/feeds/cells/1arXjPy5iUERug2miLIPZKs0equJKxcT-QMqika16gm0/1/public/full?alt=json';
+	let url = 'https://spreadsheets.google.com/feeds/cells/1arXjPy5iUERug2miLIPZKs0equJKxcT-QMqika16gm0/1/pu	blic/full?alt=json';
 	let obj = await fetch(url)
 	resolve(obj.json())
 })}
@@ -16,28 +16,49 @@ async function load() {
    	var confessioncomp = confessionentry
    	confessionlist.push(confessioncomp)
     }
+
 for(var i = 0; i < confessionlist.length; i++){
 	var lengthtest = confessionlist[i].length;
-	try 
+
+  //pass//
+  
+if(lengthtest < 3){
+
+	confessionlist.splice(i, 1)	
+}}
+try 
 	{ 	
   	var indexed0 = confessionlist.indexOf("deletedate");
-  	var indexed1 = confessionlist.indexOf("confession");
+  	var indexed1 = confessionlist.indexOf("Confession");
   	confessionlist.splice(indexed0,1)
   	confessionlist.splice(indexed1,1)
 	}catch(e){
-  //pass//
+  //pass
   }
+console.log(confessionlist)
+let lastNum;
+function getRandNumber() {
+  var gennedran = Math.floor(Math.random() * lengthoflist); // get new random number
 
-if(lengthtest < 3){
-	console.log("FDFS")
-  console.log(confessionlist[i])
-	confessionlist.splice(i, 1)	
-}}
-//console.log(confessionlist)    
-
-var lengthoflist = confessionlist.length
-var listselect = Math.floor(Math.random() * lengthoflist)
-document.getElementById('textinput').value =  ""
-document.getElementById('textinput').value =  confessionlist[listselect]
-console.log("TRUE")
+  if (gennedran === lastNum) { // compare with last number
+    return getRandNumber() // if they are the same, call the function again to repeat the process
+  }
+  return gennnedran; // if they're not the same, return it
 }
+function getGenned() {
+   const number = getRandNumber()
+   lastNum = number 
+   return number
+}
+console.log(confessionlist[getGenned()])
+if(confessionlist.length > 1){ 
+document.getElementById('textinput').value =  ""
+document.getElementById('textinput').value =  confessionlist[getGenned()]
+}else{
+document.getElementById('textinput').value =  ""
+document.getElementById('textinput').value =  confessionlist[0]
+}
+console.log("Completed")
+}
+load()
+
