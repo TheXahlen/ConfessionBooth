f751be4c2d6f7f8fdeae86fdeff17d30658a415b
XplosiveX <47333926+XplosiveX@users.noreply.github.com>
2021-02-18 18:44:37 -0600

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -1,40 +1,42 @@
 
-
 async function apicall(){
-    return new Promise(async(resolve,reject)=>{
-        let url = 'https://spreadsheets.google.com/feeds/cells/1arXjPy5iUERug2miLIPZKs0equJKxcT-QMqika16gm0/1/public/full?alt=json';
-        let obj = await fetch(url)
-        resolve(obj.json())
-    })
-
-}
-
+	return new Promise(async(resolve,reject)=>{
+	let url = 'https://spreadsheets.google.com/feeds/cells/1arXjPy5iUERug2miLIPZKs0equJKxcT-QMqika16gm0/1/pu	blic/full?alt=json';
+	let obj = await fetch(url)
+	resolve(obj.json())
+})}
+    
 async function load() {
 	var confessionlist = []
 	var anything = await apicall()
-    console.log(anything)
-    var textjs = anything.feed.entry
-    var yes = textjs["id"]
-    for (let i = 1; i < textjs.length; i++) {
-    var confessionentry = textjs[i].content.$t
-    var confessioncomp = confessionentry
-    confessionlist.push(confessioncomp)
-    console.log(confessionlist)    
-	for(i in confessionlist){
+  var textjs = anything.feed.entry
+  var yes = textjs["id"]
+  for (let i = 1; i < textjs.length; i++) {
+   	var confessionentry = textjs[i].content.$t
+   	var confessioncomp = confessionentry
+   	confessionlist.push(confessioncomp)
+    }
+for(var i = 0; i < confessionlist.length; i++){
 	var lengthtest = confessionlist[i].length;
-	if(lengthtest < 3){
-	confessionlist.splice(confessionlist[i], 1)	
-}    
+	try 
+	{ 	
+  	var indexed0 = confessionlist.indexOf("deletedate");
+  	var indexed1 = confessionlist.indexOf("Confession");
+  	confessionlist.splice(indexed0,1)
+  	confessionlist.splice(indexed1,1)
+	}catch(e){
+  //pass//
+  }
 
-}
+if(lengthtest < 3){
+	console.log("FDFS")
+  console.log(confessionlist[i])
+	confessionlist.splice(i, 1)	
+}}
+//console.log(confessionlist)    
 
-	
-}
 var lengthoflist = confessionlist.length
-console.log(lengthoflist)
 var listselect = Math.floor(Math.random() * lengthoflist)
-console.log(listselect)
-
 document.getElementById('textinput').value =  ""
 document.getElementById('textinput').value =  confessionlist[listselect]
 console.log("TRUE")
