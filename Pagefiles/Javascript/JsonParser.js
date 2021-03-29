044072d72129749c61a6cc86a4cf7c266c5ee2d7
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 15:53:16 -0500

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -29,7 +29,6 @@ confessionlist.splice(0,3)
 //debugging purposes
 //console.log(confessionlist, "FOR LOOP")
 //console.log(confessionlist[i].length, "LENGTH OF ITEMS")
-}}
 try 
 	{ 	
   	var indexed0 = confessionlist.indexOf("deletedate");
