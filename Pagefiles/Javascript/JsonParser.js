1d07dd8009bb1a7a5c89f512ae8ec2bf87d5061f
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 16:01:03 -0500

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -18,7 +18,7 @@ async function load() {
    	var confessioncomp = confessionentry
 	//uncomment to debug the size of submissions.
 	//console.log(confessioncomp.length, confessioncomp)
-	if(confessioncomp.length < 14){
+	if(confessioncomp.length < 20){
 	brenderlist.push(confessioncomp)	
 	}else{
 	brenderlist.push(confessioncomp)
