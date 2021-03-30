15208cc367cbd045d902a09c2cfe67c23288dd7b
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 19:34:39 -0500

    Update Loader.js

--- Pagefiles/Javascript/Loader.js
+++ Pagefiles/Javascript/Loader.js
@@ -1,8 +1,5 @@
 // anything that needs to happen on load happens in here
 
 
-function Loader(){
-document.getElementById("cycle").disabled = true;
-document.getElementById("cycle").opacity = 0.9;
-}
+////////////
 
