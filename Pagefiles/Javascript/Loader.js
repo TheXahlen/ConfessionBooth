939176e8f15b7369641a5d3c754f9011f7e6ee70
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 19:29:55 -0500

    Update Loader.js

--- Pagefiles/Javascript/Loader.js
+++ Pagefiles/Javascript/Loader.js
@@ -6,22 +6,3 @@ document.getElementById("cycle").disabled = true;
 document.getElementById("cycle").opacity = 0.9;
 }
 
-function Clicked(){
-var cycledis = document.getElementById("cycle")
-var submitdis = document.getElementById("submit")
-var selector = document.getElementById("selector")
-if(cycledis.disabled = true){
-cycledis.disabled = false;
-submitdis.disabled = true;
-submitdis.opacity = .90;
-cycledis.opacity = 0.75;
-selector.display = "none";
-  
-} else{
-cycledis.disabled = true;
-submitdis.disabled = false;
-submitdis.opacity = 0.75;
-cycledis.opacity = 0.90; 
-selector.display =  "initial";
-  
-}}
