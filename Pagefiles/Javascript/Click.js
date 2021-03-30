32fadcf8bd517cb88e9a35b1736c7ddb356006e4
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 19:37:54 -0500

    Update Click.js

--- Pagefiles/Javascript/Click.js
+++ Pagefiles/Javascript/Click.js
@@ -3,19 +3,19 @@ var cycledis = document.getElementById("cycle")
 var submitdis = document.getElementById("submit")
 var selector = document.getElementById("selector")
 if(cycledis.disabled = true){
-cycledis.disabled = "false";
-submitdis.disabled = "true";
+cycledis.disabled = false;
+submitdis.disabled = true;
 submitdis.opacity = .90;
 cycledis.opacity = 0.75;
-selector.display = "none";
+selector.display = none;
   
 } 
   
 if (submitdis.disabled = true)   {
-cycledis.disabled = "true";
-submitdis.disabled = "false";
+cycledis.disabled = true;
+submitdis.disabled = false;
 submitdis.opacity = 0.75;
 cycledis.opacity = 0.90; 
-selector.display =  "initial";
+selector.display =  initial;
   
 }}
