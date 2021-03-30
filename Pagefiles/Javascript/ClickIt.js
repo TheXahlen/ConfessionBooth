7831542edfa5bcef5e7293a3ce15ca959771cdf1
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-03-29 19:05:30 -0500

    Update ClickIt.js

--- Pagefiles/Javascript/ClickIt.js
+++ Pagefiles/Javascript/ClickIt.js
@@ -1,3 +1,19 @@
 function clickit(){
-document.getElementById("submit").click();
+var cycledis = document.getElementById("cycle")
+var submitdis = document.getElementById("submit")
+var selector = document.getElementById("selector")
+if(cycledis.disabled = true){
+cycledis.disabled = false;
+submitdis.disabled = true;
+submitdis.opacity = .90;
+cycledis.opacity = 0.75;
+selector.display = "none";
+  
+} else{
+cycledis.disabled = true;
+submitdis.disabled = false;
+submitdis.opacity = 0.75;
+cycledis.opacity = 0.90; 
+selector.display =  "initial";
+  
 }
