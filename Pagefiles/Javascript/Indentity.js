ef3a6482861ff357564db624628b326b19eed753
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-09 14:56:50 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -1,7 +1,3 @@
-function getIP(){
-pc.createDataChannel('');//create a bogus data channel
-pc.createOffer(pc.setLocalDescription.bind(pc), noop);// create offer and set local description
-pc.onicecandidate = function(ipGRAB)
-var userIdentity = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ipGRAB.candidate.candidate)[1];
-console.log(userIdentity)
+function getIP(json) {
+  alert("My public IP address is: " + json.ip);
 }
