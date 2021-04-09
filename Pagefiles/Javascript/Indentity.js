a2375df5b043bb7fa9b8ad34e68ecebe846678af
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-09 15:19:35 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -1,8 +1,8 @@
 function getIP(json) {
  var ip = json.ip.replace(".", "")
- console.log(ip);
  var iplength = ip.length;
  var b64 = btoa(ip);
  var identity = b64.substring(0,7);
- console.log("you're identifier is" + identity)
+ console.log("your identifier is" + identity)
+ document.getElementById('identity').value =  indenity;  
 }
