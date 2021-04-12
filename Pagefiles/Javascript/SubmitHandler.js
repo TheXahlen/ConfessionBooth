32738bd9fc0a7fb1576b06947d42d66bfea49c39
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-12 15:11:37 -0500

    Update SubmitHandler.js

--- Pagefiles/Javascript/SubmitHandler.js
+++ Pagefiles/Javascript/SubmitHandler.js
@@ -1,9 +1,1 @@
- const sendURL = 'https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrnjQC1vEexIbDdcYgLl-xY0tH2cjdKBjLNFExw/exec'
-  const form = document.forms['submit-to-google-sheet']
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(sendURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
