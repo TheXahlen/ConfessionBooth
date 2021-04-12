005779800852b16611e938b2ae3b2366a75f4f90
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-12 15:16:38 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -5,6 +5,6 @@ function getIP(json) {
  var identity = b64.substring(0,7);
  document.getElementById('identity').value =  identity;
  console.log("your identifier is" + identity)
- document.getElementById('posterid').value =  identity; 
+ document.getElementById('posterid').innerHTML =  identity; 
  console.log("document has updated the user identity")
 }
