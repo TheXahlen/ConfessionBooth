56f6f10095ba91c9425a7620427493f0f510d8a9
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-12 20:17:55 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -1,10 +1,14 @@
-var blacklist = []
+
 function getIP(json) {
+ // THIS IS THE bLACKLIST FOR USERS WHO SHOULDNT USE IT//
+ var blacklist = []
+ // hiIII///
  var ip = json.ip.replace(".", "")
  var iplength = ip.length;
  var b64 = btoa(ip);
  var identity = b64.substring(0,7);
- if(blacklist.includes(identity) = true){
+ var testif = blacklist.includes(identity);
+ if( testif == true){
  document.getElementById('bodyifallowed').style.display = "none"; 
  document.getElementById('bodyifnotallowed').style.display = "block";
  }else{
