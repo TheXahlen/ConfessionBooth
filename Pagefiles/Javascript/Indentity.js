294c5ef95caf96fd8439417ce2677f858cab3195
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-13 15:25:37 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -1,15 +1,31 @@
 
 function getIP(json) {
+ function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789:}{[]-=+)(*&^%$#@!.,/?';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
  // THIS IS THE bLACKLIST FOR USERS WHO SHOULDNT USE IT//
  //explample blew of how to black list a user
  //var blacklist = ["sASfdsfgb","dsfsAAAA","DSAFDSAaaaa"]
  var blacklist = [""]
+ try{
+ var identity = localStorage.getItem("browserid")
+ console.log("ID FOUND AND LOADED: " + identity
+ }catch{
+ console.log("YOU HAVE NO USER ID GENERATING")
  var jsonip = JSON.stringify(json.ip)
- var iplength = jsonip.length;
+ var iplength = jsonip.length - 3;
+ var extensionadd = makeid(5);
  var b64 = btoa(jsonip);
- var identity = b64.substring(0,iplength).split("").reverse().join("");
- //future browser based blocking to stop vpn
+ var identity = "{" + b64.substring(0,iplength).split("").reverse().join("") + "}{" + extensionadd + "}";
+ console.log("USER ID GENERATED: " + identity;
  localStorage.setItem("browserid", identity);
+ }
  var testif = blacklist.includes(identity);
  console.log("if true you are blocked sorry: " + testif)
  //
