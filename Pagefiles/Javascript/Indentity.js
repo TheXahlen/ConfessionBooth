89ebd40cbdba6f4ee0ba2f021c0c5dd2d5d794e6
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-12 20:10:32 -0500

    Update Indentity.js

--- Pagefiles/Javascript/Indentity.js
+++ Pagefiles/Javascript/Indentity.js
@@ -1,10 +1,15 @@
+var blacklist = []
 function getIP(json) {
  var ip = json.ip.replace(".", "")
  var iplength = ip.length;
  var b64 = btoa(ip);
  var identity = b64.substring(0,7);
+ if(blacklist.includes(identity) = true){
+ document.getElementById('bodyifallowed').style.display = "none"; 
+ document.getElementById('bodyifnotallowed').style.display = "block";
+ }else{
  document.getElementById('identity').value =  identity;
  console.log("your identifier is" + identity)
  document.getElementById('posterid').innerHTML =  identity; 
  console.log("document has updated the user identity")
-}
+}}
