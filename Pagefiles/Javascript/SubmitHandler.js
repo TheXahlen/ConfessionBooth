f01afbb80ce4c5b0aa07f9e1db501af41ffde81d
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 00:58:26 -0500

    Update SubmitHandler.js

--- Pagefiles/Javascript/SubmitHandler.js
+++ Pagefiles/Javascript/SubmitHandler.js
@@ -3,6 +3,6 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrn
     form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response) alert("Your post was submitted!"))
-      .catch(error => console.error('Error!', error.message) alert("Something went wrong and your post was discarded!"))
+      .then(response =>  alert("Your post was submitted!"))
+      .catch(error =>  alert("Something went wrong and your post was discarded!"))
     })
