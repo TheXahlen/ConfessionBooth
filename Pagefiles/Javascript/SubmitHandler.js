55c0e97c1c5948b519dbf3c0621c9e3413865d7c
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 13:47:20 -0500

    Update SubmitHandler.js

--- Pagefiles/Javascript/SubmitHandler.js
+++ Pagefiles/Javascript/SubmitHandler.js
@@ -1,8 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrnjQC1vEexIbDdcYgLl-xY0tH2cjdKBjLNFExw/exec'
+    function success(res){
+    alert("Your post was submitted!")
+    console.log(console.log('Success!', res))
+    }
+    function failed(res){
+    alert("Something went wrong and your post was discarded!")
+    console.error('Error!', res)
+    }
     const form = document.forms['submit-to-google-sheet']
     form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response =>  alert("Your post was submitted!"))
-      .catch(error =>  alert("Something went wrong and your post was discarded!"))
+      .then(response =>  success(response))
+      .catch(error =>  failed(error.message))
     })
