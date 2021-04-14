e5adbe01dec9ac42dbba3c43d58cff2822b0b2bf
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 00:53:28 -0500

    Update Disgen.js

--- Pagefiles/Javascript/Disgen.js
+++ Pagefiles/Javascript/Disgen.js
@@ -20,9 +20,11 @@ var amount = indexes.length;
 var listofposts = []
 //return "This user identifier| " + browseridvar + " | has | " + amount + " | posts with an id at the following indexes: {" + indexes + "}";
 for(var b = 0; b < indexes.length; b++){
-listofposts.push("Post number {" + b + "} [" + brenderparsedvar[indexes[b] - 4] + " ]")
+listofposts.push("Post number {" + b + "} [" + brenderparsedvar[indexes[b] - 4] + " ] " + "\n")
 
 }
 console.log(listofposts + ": COMPLETE LIST! FOR USER WITH ID : {" + currentuserID + "}")
 document.getElementById('textinput').value =  listofposts;
+document.getElementById('textinput').style.color = "black";
+document.getElementById('textinput').style.textDecoration =  "none";
 }
