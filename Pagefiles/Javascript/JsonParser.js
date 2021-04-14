21c4e0bf1800642b0c2b54db157648ea9278bbf0
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 13:48:52 -0500

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -14,26 +14,26 @@ async function load() {
 	var confessionlist = []
 	var brenderlist = []
 	var anything = await apicall()
-  var textjs = anything.feed.entry
-  var yes = textjs["id"]
-  for (let i = 1; i < textjs.length; i++) {
-   	var confessionentry = textjs[i].content.$t
-   	var confessioncomp = confessionentry
-	//uncomment to debug the size of submissions.
-	//console.log(confessioncomp.length, confessioncomp
-	if(confessioncomp == "identity" || confessioncomp == "font" || confessioncomp == "confession" || confessioncomp == "deletedate" || confessioncomp == "color" || confessioncomp == "decoration" || confessioncomp == "size"){
-	 console.log("filtered collumn header: ", confessioncomp)
-	   }else{
-	if(confessioncomp.length < 25){
-	brenderlist.push(confessioncomp)	
-	}else{
-	brenderlist.push(confessioncomp)
-   	confessionlist.push(confessioncomp)
-	}}
-  localStorage.setItem("brenderlist", JSON.stringify(brenderlist));
-  localStorage.setItem("confessions", JSON.stringify(confessionlist));
-  console.log("BRENDER META DATA LIST LOADED with {", brenderlist.length, "} Items" )
-  console.log("CONFESSIONLIST LOADED with {", confessionlist.length, "}items here it is mutiplied by 8 {", confessionlist.length * 8 - 9,"} if this matched the BRENDER meta data above everything is in correct index!")
-  console.log("new list structure loaded with out complaint! this is the {" + loadcount + "} time a new data set has been loaded since page was opened")
-  }}
+	var textjs = anything.feed.entry
+	var yes = textjs["id"]
+	for (let i = 1; i < textjs.length; i++) {
+		var confessionentry = textjs[i].content.$t
+		var confessioncomp = confessionentry
+		//uncomment to debug the size of submissions.
+		//console.log(confessioncomp.length, confessioncomp
+		if(confessioncomp == "identity" || confessioncomp == "font" || confessioncomp == "confession" || confessioncomp == "deletedate" || confessioncomp == "color" || confessioncomp == "decoration" || confessioncomp == "size"){
+			 console.log("filtered collumn header: ", confessioncomp)
+		}else{
+			if(confessioncomp.length < 25){
+				brenderlist.push(confessioncomp)	
+			}else{
+				brenderlist.push(confessioncomp)
+				confessionlist.push(confessioncomp)
+			}}}
+localStorage.setItem("brenderlist", JSON.stringify(brenderlist));
+localStorage.setItem("confessions", JSON.stringify(confessionlist));
+console.log("BRENDER META DATA LIST LOADED with {", brenderlist.length, "} Items" )
+console.log("CONFESSIONLIST LOADED with {", confessionlist.length, "}items here it is mutiplied by 8 {", confessionlist.length * 8 - 9,"} if this matched the BRENDER meta data above everything is in correct index!")
+console.log("new list structure loaded with out complaint! this is the {" + loadcount + "} time a new data set has been loaded since page was opened")
+}
 
