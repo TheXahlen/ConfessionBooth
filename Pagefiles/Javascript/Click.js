476385c7e0b4cfe0a0321d629335e0217cda292d
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 01:59:52 -0500

    Update Click.js

--- Pagefiles/Javascript/Click.js
+++ Pagefiles/Javascript/Click.js
@@ -5,8 +5,18 @@
 //var selector = document.getElementById("selector")
 
 ///file is now repurposed.
+//Originally used to switch between a submit and cycle mode
+//couldn't figure out how to get it to work so now this clicks the button within the form
+//the reason this clicks an invisble button is the only way the listener detects if you are actually submitting
+//is if the button that is clicked exists within the <form> </form> so this made it impossible to align the submit and cycle 
+// buttons also if I aligned them within the <form> </form> than cycle also submitted data sadly. So I just made this click an
+//invisible button inside the <form> </form>
 
 function Clicked(){
+var currenttextread = document.getElementById("currenttext");
+if(currenttextread == document.getElementById("textinput"){
+alert("Please don't submit other peoples messages, or things you just cycled through thanks!")
+}else{
 document.getElementById("submit").click();
-}
+}}
 
