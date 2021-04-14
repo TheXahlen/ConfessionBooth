e76225c3d52c8035a7695aace50486189ee1a38e
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 15:20:52 -0500

    Update Loader.js

--- Pagefiles/Javascript/Loader.js
+++ Pagefiles/Javascript/Loader.js
@@ -1,6 +1,7 @@
 // anything that needs to happen on load happens in here
 function Loader(){
 localStorage.setItem("FIRSTRUN","TRUE")
+localStorage.setItem("LOADED","FALSE")
 localStorage.setItem("numbergenned", 0);
 localStorage.setItem("numcycled",0)
 localStorage.setItem("dataload",0)
