e4be414ff1c501e3784c65e9f8cd8236f579a667
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 14:12:09 -0500

    Update Delandret.js

--- Pagefiles/Javascript/Delandret.js
+++ Pagefiles/Javascript/Delandret.js
@@ -5,26 +5,34 @@ var numbergenned = localStorage.getItem('numbergenned');
 var brenderit = localStorage.getItem('brenderlist');
 var setit = localStorage.getItem('browserid');
 var firstrun = localStorage.getItem('FIRSTRUN');
-if (listit !== null || numbergenned !== null || brenderit !== null || firstrun !== null){
-document.getElementById('listdata').innerHTML =  "TRUE";
-} else{
-document.getElementById('listdata').innerHTML =  "FALSE";
+var cyclenum = localStorage.getItem('numcycled');
+var dataload = localStorage.getItem('dataload');
+function detectPRESENCE(){
+  if (listit !== null || numbergenned !== null || brenderit !== null || firstrun !== null || dataload !== null || cyclenum !== null){
+  document.getElementById('listdata').innerHTML =  "TRUE";
+  } else{
+  document.getElementById('listdata').innerHTML =  "FALSE";
+  }
+  if (setit == null){
+  document.getElementById('browserid').innerHTML =  "NULL"
+  }else{
+  document.getElementById('browserid').innerHTML =  setit;
+  }}catch{
+  document.getElementById('browserid').innerHTML =  "NULL";  
+  }}
 }
-if (setit == null){
-document.getElementById('browserid').innerHTML =  "NULL"
-}else{
-document.getElementById('browserid').innerHTML =  setit;
-}}catch{
-document.getElementById('browserid').innerHTML =  "NULL";  
-}}
+
 function removeData(){
 localStorage.removeItem('browserid');
-document.getElementById('browserid').innerHTML =  "NULL";
+detectPRESENCE()
 }
 function removelistData(){
 localStorage.removeItem('brenderlist');
 localStorage.removeItem('confessions');
 localStorage.removeItem('numbergenned');
-document.getElementById('listdata').innerHTML =  "FALSE";
+localStorage.removeItem('FIRSTRUN');
+localStorage.removeItem('numcycled');
+localStorage.removeItem('dataload');
+detectPRESENCE()
 }
 
