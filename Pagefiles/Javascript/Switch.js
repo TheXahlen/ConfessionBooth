e891ebd8879e2865ab45802afa9553c2c7882c94
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 04:50:34 -0500

    Update Switch.js

--- Pagefiles/Javascript/Switch.js
+++ Pagefiles/Javascript/Switch.js
@@ -1,19 +1,26 @@
 //file for switching 
 function switchd(){
-   //var cycledis = document.getElementById("cycle").style.display
-   //var subdis = document.getElementById("subdis").style.display
+   
+   
+   var cycledis = document.getElementById("cycle").style.display
+   var subdis = document.getElementById("subtry").style.display
+   var textdis = document.getElementById("textinput").style.display
+   var selectdis =  document.getElementById("selector").style.display
+   var textsubdis = document.getElementById("textsubmit").style.display
+   
+   
    if (cycledis == "none"){
-   document.getElementById("cycle").style.display = "block"
-   document.getElementById("textinput").style.display = "block"
-   document.getElementById("subtry").style.display = "none"
-   document.getElementById("selector").style.display = "none"
-   document.getElementById("textsubmit").style.display = "none"
-   localStorage.setItem("buttonDescribe",'<button onclick="loadPosts()" class="postid" style="display:block"> || View This Users Posts </button>')
+   cycledis = "block"
+   textdis = "block"
+   subdis = "none"
+   selectdis = "none"
+   textsubdis = "none"
+   //localStorage.setItem("buttonDescribe",'<button onclick="loadPosts()" class="postid" style="display:block"> || View This Users Posts </button>')
    }else{
-   document.getElementById("cycle").style.display = "none"
-   document.getElementById("textinput").style.display = "none"
-   document.getElementById("textsubmit").style.display = "block"
-   document.getElementById("subtry").style.display = "block"
-   document.getElementById("selector").style.display = "block"
-   localStorage.setItem("buttonDescribe",'<button onclick="loadPosts()" class="postid" style="display:none"> || View This Users Posts </button>')
+   cycledis = "none"
+   textdis = "none"
+   textsubdis = "block"
+   subdis = "block"
+   selectdis = "block"
+   //localStorage.setItem("buttonDescribe",'<button onclick="loadPosts()" class="postid" style="display:none"> || View This Users Posts </button>')
    }}
