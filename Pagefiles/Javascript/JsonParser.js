7c92697018b2b51bdd141d398cee28199834ed61
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 14:25:16 -0500

    Update JsonParser.js

--- Pagefiles/Javascript/JsonParser.js
+++ Pagefiles/Javascript/JsonParser.js
@@ -34,6 +34,6 @@ localStorage.setItem("brenderlist", JSON.stringify(brenderlist));
 localStorage.setItem("confessions", JSON.stringify(confessionlist));
 console.log("BRENDER META DATA LIST LOADED with {", brenderlist.length, "} Items" )
 console.log("CONFESSIONLIST LOADED with {", confessionlist.length, "}items here it is mutiplied by 8 {", confessionlist.length * 8,"} if this is within an offset of 15 from the BRENDER meta data above everything is in correct index!")
-console.log("new list structure loaded with out complaint! this is the {" + loadcount + "} time a new data set has been loaded since page was opened")
+console.log("new list structure loaded with out complaint! this is the {" + intPARSE(localStorage.getItem("dataload")) + "} time a new data set has been loaded since page was opened")
 }
 
