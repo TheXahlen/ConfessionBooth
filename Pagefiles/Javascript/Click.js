307ff0e61446476ee8630e1672ee9956a605d82b
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 11:31:03 -0500

    Update Click.js

--- Pagefiles/Javascript/Click.js
+++ Pagefiles/Javascript/Click.js
@@ -37,8 +37,9 @@ if(loadedposts.length > 0){
     for(var j = 0; j < amountforj; j++){
     var similarity = stringSimilarity.compareTwoStrings(loadedposts[j], currenttextinsubmit);
     console.log("Comparing similarity of" , loadedposts[j] ,  ":to:" , currenttextinsubmit , "found a similarity of: {", similarity ,"}") 
+    var errorreport = "This post was too similar to a previous post made with your identifer. Give it another go! \n " + loadedposts[j] +  " :compared with: " + currenttextinsubmit + "found a similarity of: {" + similarity + "} similarity limit of 0.75"
     if(similarity > 0.75){
-       alert("This post was too similar to a previous post made with your identifer. Give it another go! \n" , loadedposts[j] ,  ":compared with:" , currenttextinsubmit , "found a similarity of: {", similarity ,"} similarity limit of 0.75")
+       alert(errorreport)
        return }
     }}
 
