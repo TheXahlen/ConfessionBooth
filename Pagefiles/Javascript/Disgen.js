90a8d599a6986cf79e8a4b58c293d3d006cabdc0
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-14 19:18:57 -0500

    Update Disgen.js

--- Pagefiles/Javascript/Disgen.js
+++ Pagefiles/Javascript/Disgen.js
@@ -30,16 +30,19 @@ var textdecoration = brenderparsedvar[indexes[b] - 3] + ';'
 var textcolor = brenderparsedvar[indexes[b] - 4] + ';'
 var postmodifer = '<p ' + 'style="font-size:' +  sizeoffont + 'font-family:' + fontfamily + 'text-decoration:' + textdecoration + 'color:' + textcolor + '">'  
 var postnum = '<b style="color:black">[' + b + "]</b>" + postindexed
-listofposts.push(  postmodifer + postnum + '</p> <br>')
+listofposts.push(postmodifer + postnum + '</p> <br>')
+ 
 }
+
 if(indexes.length == 0){
 console.log("no posts from id: " + currentuserID)
-document.getElementById('textinput').innerHTML = "This user has not posted yet!"; 
-document.getElementById('currenttext').innerHTML = "This user has not posted yet!"; 
+document.getElementById('textinput').innerHTML = "{" + currentuserID + "} This user Identifer has not posted yet!"; 
+document.getElementById('currenttext').innerHTML = "{" + currentuserID + "} This user Identifer has not posted yet!"; 
 }else{
 console.log("This user identifier| " + browseridvar + " | has | " + amount + " | posts with an id at the following indexes: {" + indexes + "}")
 console.log(listofposts + ": COMPLETE LIST! FOR USER WITH ID : {" + currentuserID + "}")
 document.getElementById('textinput').innerHTML =  listofposts;
+console.log(document.getElementById('textinput').innerHTML)  
 document.getElementById('currenttext').innerHTML = listofposts;
 document.getElementById('textinput').style.color = "black";
 document.getElementById('textinput').style.textDecoration =  "none";
