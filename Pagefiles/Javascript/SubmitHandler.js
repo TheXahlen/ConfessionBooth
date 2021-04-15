c58aad5b677ea2759027678811e1136094e8e6d0
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 11:59:58 -0500

    Update SubmitHandler.js

--- Pagefiles/Javascript/SubmitHandler.js
+++ Pagefiles/Javascript/SubmitHandler.js
@@ -1,18 +1,21 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrnjQC1vEexIbDdcYgLl-xY0tH2cjdKBjLNFExw/exec'
-    function success(res){
-    var cursubmission = document.getElementById("textsubmit").value
-    var sub = "Your post {" + cursubmission + "} was submitted!"
-    alert(sub)
-    document.getElementById("textsubmit").value = ""
-    console.log(console.log('Success!', res))
-    document.getElementById("submit").disabled = false;
-    load()
-    }
-    function failed(res){
-    alert("Something went wrong and your post was discarded!")
-    console.error('Error!', res)
-    document.getElementById("submit").disabled = false;
-    }
+    
+function success(res){
+        var cursubmission = document.getElementById("textsubmit").value
+        var sub = "Your post {" + cursubmission + "} was submitted!"
+        alert(sub)
+        document.getElementById("textsubmit").value = ""
+        console.log(console.log('Success!', res))
+        document.getElementById("submit").disabled = false;
+        load()
+}
+
+function failed(res){
+        alert("Something went wrong and your post was discarded!")
+        console.error('Error!', res)
+        document.getElementById("submit").disabled = false;
+}
+
     const form = document.forms['submit-to-google-sheet']
     form.addEventListener('submit', e => {
     e.preventDefault()
