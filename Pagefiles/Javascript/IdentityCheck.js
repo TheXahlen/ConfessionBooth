b89a4659e7344b7614ee12de96e5b50097fa8e19
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 22:41:09 -0500

    Update IdentityCheck.js

--- Pagefiles/Javascript/IdentityCheck.js
+++ Pagefiles/Javascript/IdentityCheck.js
@@ -1,1 +1,14 @@
 
+function checkID(){
+var idtocheck = localStorage.getItem('browserid')
+  if (idtocheck !== null){
+     var identity = localStorage.getItem("browserid")
+     } else {
+      getIP()
+      return  
+     }
+var listofint = ["1","2","3","4","5","6","7","8","9","0"]
+var idlength = idtocheck.length
+var idiptocheck = idtocheck.substring(1,8)
+var idendtocheck = idtocheck.substring(10,18)
+}
