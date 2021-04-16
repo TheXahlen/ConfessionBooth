c86e638e82726e889e9bbfc525dda209575d979c
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 23:16:00 -0500

    Update IdentityCheck.js

--- Pagefiles/Javascript/IdentityCheck.js
+++ Pagefiles/Javascript/IdentityCheck.js
@@ -11,21 +11,25 @@ var listofint = ["1","2","3","4","5","6","7","8","9","0"]
 var idlength = idtocheck.length;
 var idiptocheck = idtocheck.substring(1,8);
 idiptocheck = atob(idiptocheck);
+  console.log(idiptocheck)
 var idiplength = idtocheck.length;
  //these test if an id is actually legit.
  for (var b = 0; b < idiplength; b++) {
    var indextocheck = idiptocheck.substring(0,b);
-   if(idiplength !== 18){
-     console.log("The ID {" + idtocheck + "} is invalid due to an invalid index: GENERATING  NEW ID :")
-     localStorage.removeItem('browserid')
-     getIP()
-     return null
-      }}
-   if(listofint.includes(indextocheck) == false){
+    if(listofint.includes(indextocheck) == false){
+     console.log(indextocheck)
      console.log("The ID {" + idtocheck + "} is invalid due to tampering: GENERATING  NEW ID :")
      localStorage.removeItem('browserid')
      getIP()
      return null
+    }}
+   if(idiplength !== 18){
+     console.log("The ID {" + idtocheck + "} is invalid due to an invalid index: GENERATING  NEW ID :" )
+     localStorage.removeItem('browserid')
+     getIP()
+     return null
+      }
+
 
       }
    console.log("CONGRATS you passed your id is legit bro 😎")
