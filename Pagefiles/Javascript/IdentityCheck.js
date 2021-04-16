43001cd4ae2a9aae5b02700d3a33bce9712d9ee0
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 23:08:13 -0500

    Update IdentityCheck.js

--- Pagefiles/Javascript/IdentityCheck.js
+++ Pagefiles/Javascript/IdentityCheck.js
@@ -1,12 +1,12 @@
 
 function checkID(){
 var idtocheck = localStorage.getItem('browserid')
-  if (idtocheck !== null){
-     var identity = localStorage.getItem("browserid")
-     } else {
-      getIP()
-      return null
-     }
+  //if (idtocheck !== null){
+   //  var identity = localStorage.getItem("browserid")
+   //  } else {
+    //  getIP()
+    //  return null
+    // }
 var listofint = ["1","2","3","4","5","6","7","8","9","0"]
 var idlength = idtocheck.length;
 var idiptocheck = idtocheck.substring(1,8);
@@ -16,10 +16,10 @@ var idiplength = idiptocheck.length;
  for (var b = 0; b < idiplength; b++) {
    var indextocheck = idiptocheck.substring(0,b);
    if(idiplength !== 18){
+     console.log("The ID {" + idtocheck + "} is invalid due to an invalid index: GENERATING  NEW ID :")
      localStorage.removeItem('browserid')
      getIP()
      return null
-     console.log("The ID {" + idtocheck + "} is invalid due to an invalid index: GENERATING  NEW ID :")
       }}
    if(listofint.includes(indextocheck) == false){
      console.log("The ID {" + idtocheck + "} is invalid due to tampering: GENERATING  NEW ID :")
