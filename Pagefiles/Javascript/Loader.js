3cd4ba10b762e9fc327112be5b1deffc3bfeab73
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-15 23:44:04 -0500

    Update Loader.js

--- Pagefiles/Javascript/Loader.js
+++ Pagefiles/Javascript/Loader.js
@@ -1,11 +1,22 @@
 // anything that needs to happen on load happens in here
 function Loader(){
    
+document.getElementById("cycle").disabled = true;
+document.getElementById("cycle").backgroundColor = "grey";
+   
+document.getElementById("switcher").disabled = true;
+document.getElementById("switcher").backgroundColor = "grey";
+   
+document.getElementById("subtry").disabled = true;
+document.getElementById("subtry").backgroundColor = "grey";
+
+load()   
 localStorage.setItem("FIRSTRUN","TRUE")
 localStorage.setItem("LOADED","FALSE")
 localStorage.setItem("numbergenned", 0);
 localStorage.setItem("numcycled",0)
 localStorage.setItem("dataload",0)
+setdeleteDate();
 
 function detect(){
    if(localStorage.getItem("LOADED") == "FALSE"){
@@ -16,6 +27,15 @@ checkID()
 localStorage.setItem("browserid", identity);
 document.getElementById("currentID").innerHTML = identity;
 document.getElementById('posterid').innerHTML =  identity;
+   
+ document.getElementById("cycle").disabled = false;
+document.getElementById("cycle").backgroundColor = "white";
+   
+document.getElementById("switcher").disabled = false;
+document.getElementById("switcher").backgroundColor = "white";
+   
+document.getElementById("subtry").disabled = false;
+document.getElementById("subtry").backgroundColor = "white";  
 //pass
 }
 ////////////
