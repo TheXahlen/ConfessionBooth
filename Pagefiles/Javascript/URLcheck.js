2eb76edefd008fc5c4aa11a0371752a2711120e4
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-21 01:44:48 -0500

    Update URLcheck.js

--- Pagefiles/Javascript/URLcheck.js
+++ Pagefiles/Javascript/URLcheck.js
@@ -1,13 +1,23 @@
 function retPARAM(){
 curURL = window.location.href
 urlSET = curURL.search("=") + 1
+try{
+secondValueSET = curURL.search("/>") + 1 
+identityforsecondvalue = curURL.substring(secondValueSET,curURL.length)
+indentityforsecondlen = identityforsecondvalue.length
+identityforSEARCH = curURL.substring(urlSET,curURL.length - indentityforsecondlen)
+}catch{
 identityforSEARCH = curURL.substring(urlSET,curURL.length)
+}
 console.log(curURL)
 console.log(identityforSEARCH)
 }
 
-function editURL(name,value){
+function editURL(name,value,secondvalue){
 currentURL = "https://xplosivex.github.io/ConfessionBooth/index.html"
 window.location.href = currentURL + "?" + name + "=" + value;
+if(secondvalue !== ""){
+window.location.href = currentURL + "?" + name + "=" + value + "/>" + secondvalue + "/>"; 
+}
 retPARAM()
 }
