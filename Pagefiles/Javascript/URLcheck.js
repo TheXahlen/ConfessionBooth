73c2e81acd309b330cbdd5e0bbc917b48bf4e79d
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-21 01:58:00 -0500

    Update URLcheck.js

--- Pagefiles/Javascript/URLcheck.js
+++ Pagefiles/Javascript/URLcheck.js
@@ -1,15 +1,14 @@
 function retPARAM(){
-curURL = window.location.href
-urlSET = curURL.search("=") + 1
-try{
-secondValueSET = curURL.search("/>") + 1 
-identityforsecondvalue = curURL.substring(secondValueSET,curURL.length)
-indentityforsecondlen = identityforsecondvalue.length
-identityforSEARCH = curURL.substring(urlSET,curURL.length - indentityforsecondlen)
-}catch{
-identityforSEARCH = curURL.substring(urlSET,curURL.length)
-}
-console.log(curURL)
+var curURL = window.location.href
+var urlSecond = curURL.replace(/H.*S/, '%%')
+var urlSecondFormat = urlSecond.replaceAll("%","")
+console.log(urlSecondFormat)
+var urlSET = urlSecondFormat.search("=") + 1;
+var identityforSEARCH = curURL.substring(urlSET,urlSecondFormat.length)
+var percentind = curURL.search("%") + 1;
+var suboftotal = curURL.substring(percentind, curURL.length - 1)
+//console.log(curURL)
+console.log(suboftotal)
 console.log(identityforSEARCH)
 }
 
@@ -17,7 +16,7 @@ function editURL(name,value,secondvalue){
 currentURL = "https://xplosivex.github.io/ConfessionBooth/index.html"
 window.location.href = currentURL + "?" + name + "=" + value;
 if(secondvalue !== ""){
-window.location.href = currentURL + "?" + name + "=" + value + "/>" + secondvalue + "/>"; 
+window.location.href = currentURL + "?" + name + "=" + value + "::" + secondvalue + "::"; 
 }
 retPARAM()
 }
