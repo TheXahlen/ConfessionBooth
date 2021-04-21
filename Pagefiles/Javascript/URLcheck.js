9838892778b1194f41a4e75a074ea124feee6013
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-21 02:08:38 -0500

    Update URLcheck.js

--- Pagefiles/Javascript/URLcheck.js
+++ Pagefiles/Javascript/URLcheck.js
@@ -5,8 +5,9 @@ var urlSecondFormat = urlSecond.replaceAll("%","")
 console.log(curURL)
 var urlSET = urlSecondFormat.search("=") + 1;
 var identityforSEARCH = curURL.substring(urlSET,urlSecondFormat.length)
-var percentind = curURL.search("%") + 1;
-var suboftotal = curURL.substring(percentind, curURL.length - 2)
+var percentind = curURL.search("%");
+var suboftotal = curURL.substring(percentind, curURL.length)
+suboftotal = suboftotal.replaceAll("%","")
 //console.log(curURL)
 console.log(suboftotal)
 console.log(identityforSEARCH)
