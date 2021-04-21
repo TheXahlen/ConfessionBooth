19d4ca7950816498d82b964863968ec9f0531287
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-21 02:24:17 -0500

    Update URLcheck.js

--- Pagefiles/Javascript/URLcheck.js
+++ Pagefiles/Javascript/URLcheck.js
@@ -1,24 +1,25 @@
 function retPARAM(){
-var curURL = window.location.href
-curURL = curURL.replaceAll("#","") 
-var urlSecond = curURL.replace(/%.*%/, '%%')
-var urlSecondFormat = urlSecond.replaceAll("%","")
+curURL = window.location.href
+urlSET = curURL.search("=") + 1
+try{
+secondValueSET = curURL.search("/>") + 1 
+identityforsecondvalue = curURL.substring(secondValueSET,curURL.length)
+indentityforsecondlen = identityforsecondvalue.length
+identityforSEARCH = curURL.substring(urlSET,curURL.length - indentityforsecondlen)
+console.log("TRIED AND DID IT")
+}catch{
+console.log("CAUGHT AN ERROR")
+identityforSEARCH = curURL.substring(urlSET,curURL.length)
+}
 console.log(curURL)
-var urlSET = urlSecondFormat.search("=") + 1;
-var identityforSEARCH = curURL.substring(urlSET,urlSecondFormat.length)
-var percentind = curURL.search("%");
-var suboftotal = curURL.substring(percentind, curURL.length)
-suboftotal = suboftotal.replaceAll("%","")
-//console.log(curURL)
-console.log(suboftotal)
 console.log(identityforSEARCH)
 }
 
 function editURL(name,value,secondvalue){
-
-parent.location.hash = currentURL + "?" + name + "=" + value;
+//var currentURL = "https://xplosivex.github.io/ConfessionBooth/index.html"
+window.location.href =  "?" + name + "=" + value;
 if(secondvalue !== ""){
-praent.location.hash = currentURL + "?" + name + "=" + value + "%" + secondvalue + "%"; 
+window.location.href = "?" + name + "=" + value + "%" + secondvalue + "%"; 
 }
 retPARAM()
 }
