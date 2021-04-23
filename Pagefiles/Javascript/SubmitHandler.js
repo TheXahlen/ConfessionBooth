d60ae51b3ff700c9c15a193082713fb517136dc0
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-23 09:33:13 -0500

    Update SubmitHandler.js

--- Pagefiles/Javascript/SubmitHandler.js
+++ Pagefiles/Javascript/SubmitHandler.js
@@ -1,27 +1,27 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrnjQC1vEexIbDdcYgLl-xY0tH2cjdKBjLNFExw/exec'
-    
-function success(res){
-        var cursubmission = document.getElementById("textsubmit").value
-        var sub = "Your post {" + cursubmission + "} was submitted!"
-        alert(sub)
-        document.getElementById("textsubmit").value = ""
-        console.log(console.log('Success!', res))
-        document.getElementById("subtry").disabled = false;
-        document.getElementById("subtry").backgroundColor = "white";
-        load()
+const scriptURL = "https://script.google.com/macros/s/AKfycbyhferKoM3lawXmfUHRrnjQC1vEexIbDdcYgLl-xY0tH2cjdKBjLNFExw/exec";
+
+function success(res) {
+    var cursubmission = document.getElementById("textsubmit").value;
+    var sub = "Your post {" + cursubmission + "} was submitted!";
+    alert(sub);
+    document.getElementById("textsubmit").value = "";
+    console.log(console.log("Success!", res));
+    document.getElementById("subtry").disabled = false;
+    document.getElementById("subtry").backgroundColor = "white";
+    load();
 }
 
-function failed(res){
-        alert("Something went wrong and your post was discarded!")
-        console.error('Error!', res)
-        document.getElementById("subtry").disabled = false;
-        document.getElementById("subtry").backgroundColor = "white";
+function failed(res) {
+    alert("Something went wrong and your post was discarded!");
+    console.error("Error!", res);
+    document.getElementById("subtry").disabled = false;
+    document.getElementById("subtry").backgroundColor = "white";
 }
 
-    const form = document.forms['submit-to-google-sheet']
-    form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response =>  success(response))
-      .catch(error =>  failed(error.message))
-    })
+const form = document.forms["submit-to-google-sheet"];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => success(response))
+        .catch((error) => failed(error.message));
+});
