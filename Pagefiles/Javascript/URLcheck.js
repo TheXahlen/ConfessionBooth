9fe34eecd35932ad5e8d05eebcf72a1acf9c20c9
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-23 09:34:06 -0500

    Update URLcheck.js

--- Pagefiles/Javascript/URLcheck.js
+++ Pagefiles/Javascript/URLcheck.js
@@ -1,27 +1,28 @@
-function retPARAM(){
-var curURL = window.location.href
-curURL = curURL.replaceAll("#","") 
-var urlSecond = curURL.replace(/%.*%/, '%%')
-var urlSecondFormat = urlSecond.replaceAll("%","")
-console.log(curURL)
-var urlSET = urlSecondFormat.search("=") + 1;
-var identityforSEARCH = curURL.substring(urlSET,urlSecondFormat.length)
-var percentind = curURL.search("%");
-var suboftotal = curURL.substring(percentind, curURL.length)
-suboftotal = suboftotal.replaceAll("%","")
-if(identityforSEARCH ==  "https://xplosivex.github.io/ConfessionBooth/index.html"){
-return ["",""]
-}else{  
-return [suboftotal,identityforSEARCH] 
-}
-//console.log(suboftotal)
-//console.log(identityforSEARCH)
+function retPARAM() {
+    var curURL = window.location.href;
+    curURL = curURL.replaceAll("#", "");
+    var urlSecond = curURL.replace(/%.*%/, "%%");
+    var urlSecondFormat = urlSecond.replaceAll("%", "");
+    console.log(curURL);
+    var urlSET = urlSecondFormat.search("=") + 1;
+    var identityforSEARCH = curURL.substring(urlSET, urlSecondFormat.length);
+    var percentind = curURL.search("%");
+    var suboftotal = curURL.substring(percentind, curURL.length);
+    suboftotal = suboftotal.replaceAll("%", "");
+    if (identityforSEARCH == "https://xplosivex.github.io/ConfessionBooth/index.html") {
+        return ["", ""];
+    } else {
+        return [suboftotal, identityforSEARCH];
+    }
+    //console.log(suboftotal)
+    //console.log(identityforSEARCH)
 }
 
-function editURL(name,value,secondvalue){
-//var currentURL = "https://xplosivex.github.io/ConfessionBooth/index.html"
-parent.location.hash  =  "?" + name + "=" + value;
-if(secondvalue !== ""){
-parent.location.hash = "?" + name + "=" + value + "%" + secondvalue + "%"; 
+function editURL(name, value, secondvalue) {
+    //var currentURL = "https://xplosivex.github.io/ConfessionBooth/index.html"
+    parent.location.hash = "?" + name + "=" + value;
+    if (secondvalue !== "") {
+        parent.location.hash = "?" + name + "=" + value + "%" + secondvalue + "%";
+    }
 }
-}
+
