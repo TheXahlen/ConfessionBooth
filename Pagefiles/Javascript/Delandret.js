9f833422fc12009addb97967a1427bda96992758
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-23 09:26:08 -0500

    Update Delandret.js

--- Pagefiles/Javascript/Delandret.js
+++ Pagefiles/Javascript/Delandret.js
@@ -1,37 +1,36 @@
 
-function detectPRESENCE(){
+function detectPRESENCE() {
+    var listit = localStorage.getItem("confessions");
+    var numbergenned = localStorage.getItem("numbergenned");
+    var brenderit = localStorage.getItem("brenderlist");
+    var setit = localStorage.getItem("browserid");
+    var firstrun = localStorage.getItem("FIRSTRUN");
+    var cyclenum = localStorage.getItem("numcycled");
+    var dataload = localStorage.getItem("dataload");
 
-var listit = localStorage.getItem('confessions');
-var numbergenned = localStorage.getItem('numbergenned');
-var brenderit = localStorage.getItem('brenderlist');
-var setit = localStorage.getItem('browserid');
-var firstrun = localStorage.getItem('FIRSTRUN');
-var cyclenum = localStorage.getItem('numcycled');
-var dataload = localStorage.getItem('dataload');
-
-if (listit !== null || numbergenned !== null || brenderit !== null || firstrun !== null || dataload !== null || cyclenum !== null){
-  document.getElementById('listdata').innerHTML =  "TRUE";
-  } else{
-  document.getElementById('listdata').innerHTML =  "FALSE";
-  }
-if (setit == null){
-  document.getElementById('browserid').innerHTML =  "NULL"
-  }else{
-  document.getElementById('browserid').innerHTML =  setit;
-}}
-
-function removeData(){
-localStorage.removeItem('browserid');
-detectPRESENCE()
-}
- 
-function removelistData(){
-localStorage.removeItem('brenderlist');
-localStorage.removeItem('confessions');
-localStorage.removeItem('numbergenned');
-localStorage.removeItem('FIRSTRUN');
-localStorage.removeItem('numcycled');
-localStorage.removeItem('dataload');
-detectPRESENCE()
+    if (listit !== null || numbergenned !== null || brenderit !== null || firstrun !== null || dataload !== null || cyclenum !== null) {
+        document.getElementById("listdata").innerHTML = "TRUE";
+    } else {
+        document.getElementById("listdata").innerHTML = "FALSE";
+    }
+    if (setit == null) {
+        document.getElementById("browserid").innerHTML = "NULL";
+    } else {
+        document.getElementById("browserid").innerHTML = setit;
+    }
 }
 
+function removeData() {
+    localStorage.removeItem("browserid");
+    detectPRESENCE();
+}
+
+function removelistData() {
+    localStorage.removeItem("brenderlist");
+    localStorage.removeItem("confessions");
+    localStorage.removeItem("numbergenned");
+    localStorage.removeItem("FIRSTRUN");
+    localStorage.removeItem("numcycled");
+    localStorage.removeItem("dataload");
+    detectPRESENCE();
+}
