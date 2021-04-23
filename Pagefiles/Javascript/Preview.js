0a258dc45625dcff020ab811c17143524395c5fc
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-23 09:32:48 -0500

    Update Preview.js

--- Pagefiles/Javascript/Preview.js
+++ Pagefiles/Javascript/Preview.js
@@ -1,20 +1,20 @@
 function SelectedColor() {
-var e = document.getElementById("color");
-var strUser = e.options[e.selectedIndex].text;
-document.getElementById("Display").style.color = strUser;
+    var e = document.getElementById("color");
+    var strUser = e.options[e.selectedIndex].text;
+    document.getElementById("Display").style.color = strUser;
 }
-function SelectedDecoration(){
-var e = document.getElementById("decoration");
-var strUser = e.options[e.selectedIndex].text;
-document.getElementById("Display"").textDecoration = strUser;
+function SelectedDecoration() {
+    var e = document.getElementById("decoration");
+    var strUser = e.options[e.selectedIndex].text;
+    document.getElementById("Display").textDecoration = strUser;
 }
-function SelectedSize(){
-var e = document.getElementById("size");
-var strUser = e.options[e.selectedIndex].text;
-document.getElementById("Display").fontSize = strUser;
+function SelectedSize() {
+    var e = document.getElementById("size");
+    var strUser = e.options[e.selectedIndex].text;
+    document.getElementById("Display").fontSize = strUser;
 }
-function SelectedFont(){
-var e = document.getElementById("font");
-var strUser = e.options[e.selectedIndex].text;
-document.getElementById("Display").fontFamily = strUser;
+function SelectedFont() {
+    var e = document.getElementById("font");
+    var strUser = e.options[e.selectedIndex].text;
+    document.getElementById("Display").fontFamily = strUser;
 }
