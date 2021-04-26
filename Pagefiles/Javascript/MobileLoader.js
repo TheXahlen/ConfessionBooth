aa7a456add52f7f632b175d9e7d4a6ad3d9df161
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-26 14:18:15 -0500

    Update MobileLoader.js

--- Pagefiles/Javascript/MobileLoader.js
+++ Pagefiles/Javascript/MobileLoader.js
@@ -1,11 +1,11 @@
 function Mobile() {
-    var mobileon = "https://github.com/XplosiveX/ConfessionBooth/edit/main/mobileindex.html";
-    var detect = window.location.href;
-    if (detect == mobileon) {
+    //var mobileon = "https://github.com/XplosiveX/ConfessionBooth/edit/main/mobileindex.html";
+    //var detect = window.location.href;
+    //if (detect == mobileon) {
         //pass
-    } else {
-        if (screen.width <= 780) {
-            document.location = "mobileindex.html";
-        }
-    }
+    //} else {
+        //if (screen.width <= 780) {
+            //document.location = "mobileindex.html";
+        //}
+   // }
 }
