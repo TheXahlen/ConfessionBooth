83220a30915bd8cea650f52bcf29f4051e032567
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-26 10:38:11 -0500

    Update Disgen.js

--- Pagefiles/Javascript/Disgen.js
+++ Pagefiles/Javascript/Disgen.js
@@ -45,8 +45,9 @@ function loadPosts(testspecific) {
         var textdecoration = brenderparsedvar[indexes[b] - 4] + "; ";
         var textcolor = brenderparsedvar[indexes[b] - 5] + "; ";
         var numpos = '<b style="color:black;">[' + b + "]</b>";
+        var postpostedwhen = '<b style="color:black;"> {Posted: ' dateforpost + ' }</b>'
         var postmodifer = '<p style="line-height:100%; font-size:' + sizeoffont + "font-family:" + fontfamily + "text-decoration:" + textdecoration + "color:" + textcolor + '"> ' + numpos;
-        var pushable = postmodifer + postindexed + "{Posted:" + dateforpost + "} </p><br>";
+        var pushable = postmodifer + postindexed + postpostedwhen +  "</p><br>";
         var pushable = pushable.toString();
         listofposts.push(pushable);
         completelist.push(brenderparsedvar[indexes[b] - 7]);
