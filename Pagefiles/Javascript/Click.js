6c275315fae2b64c5c5bef7a68311851542292b3
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-26 02:58:58 -0500

    Update Click.js

--- Pagefiles/Javascript/Click.js
+++ Pagefiles/Javascript/Click.js
@@ -23,7 +23,9 @@
 //if(testforsim > 0.70){
 //alert("Please don't submit other peoples messages, or things you just cycled through thanks!")
 function contribute() {
+ console.log("CHANGED")
  var transfer = document.getElementById("textsubmit").value;
+ console.log(transfer)
  document.getElementById("confession").value = transfer;    
 }
 
