82eb804c24145b58ee65b4c10f312e9dde66d5fe
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-26 09:40:22 -0500

    Update DeleteDate.js

--- Pagefiles/Javascript/DeleteDate.js
+++ Pagefiles/Javascript/DeleteDate.js
@@ -6,3 +6,16 @@ function setdeleteDate() {
     }
     document.getElementById("deletedate").value = deleteDate;
 }
+
+function setdeleteDate() {
+    let date = new Date();  
+    let options = {  
+     month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit"  
+    };  
+
+    var dateVal = date.toLocaleTimeString("en-us", options); 
+    dateVal = dateVal.replaceAll(",","").replaceAll(" ","-")
+    var completedDate = "{" + dateVal + "}"
+    document.getElementById("currentdate").value = completedDate;
+}
+
