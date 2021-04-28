29e077fe5c7cabe3d169f6caa1cad3e24c83a646
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-04-28 05:00:49 -0500

    Update Checksim.js

--- Pagefiles/Javascript/Checksim.js
+++ Pagefiles/Javascript/Checksim.js
@@ -25,7 +25,7 @@ function checkSim() {
     var amountforj = loadedposts.length;
 
     if (loadedposts.length > 0) {
-        for (var j = 0; j < amountforj; j++) {
+        for (let j = 0; j < amountforj; j++) {
             var similarity = stringSimilarity.compareTwoStrings(loadedposts[j].replaceAll(" ", ""), currenttextinsubmit.replaceAll(" ", ""));
             console.log("Comparing similarity of {", loadedposts[j], "} :to: {", currenttextinsubmit, "} found a similarity of: {", similarity, "}");
             var errorreport = "This post was too similar to a previous post made with your identifer. Give it another go! \n " + loadedposts[j] + " :compared with: " + currenttextinsubmit + "found a similarity of: {" + similarity + "} similarity limit of 0.75";
